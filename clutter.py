fe6d90e288a8de69f880a8765aece9fe12309fc2
Tom Fuller <tom80f@gmail.com>
2022-06-24 10:07:31 +0100

    first commit!

---
+++ clutter.py
@@ -0,0 +1,55 @@
+
+data = {
+    "count":5,
+    "results":[{"symbol":"NFLX","name":"Netflix, Inc.","type":"stock","price":"198.98","price_open":"200.14","price_low":"198.05","price_high":"202.93","price_low_high_change_percent":"2.46","price_1d":"205.09","price_change_1d":"-6.11","price_change_percent_1d":"-2.98","price_change_percent_5d":"3.0","price_change_percent_1m":"-0.45","price_change_percent_3m":"-45.94","price_change_percent_6m":"-66.95","price_change_percent_ytd":"-66.69","price_change_percent_1y":"-59.34","price_change_percent_5y":"20.55","price_change_percent_10y":"2042.86","all_time_high_date":"2021-11-17","all_time_high_price":"691.69","all_time_high_percent_diff":"-71.23","moving_avg_50d":"258.24","moving_avg_50d_percent_diff":"-22.95","moving_avg_200d":"474.41","moving_avg_200d_percent_diff":"-58.06","volume":6989439,"volume_avg_10d":9275080,"volume_avg_10d_percent_diff":"-24.64","volume_avg_3m":12343353,"volume_avg_3m_percent_diff":"-43.37","pe_ratio":"18.06","market_cap":88401633280,"eps":"11.02","week_52_high":"700.99","week_52_high_percent_diff":"-71.61","week_52_low":"162.71","week_52_low_percent_diff":"22.29","dividend_rate":"0.0","dividend_yield_percent":"0.0","dividend_date":None,"earnings_date":"2022-04-19","analyst_rating":"2.7 - Hold","exchange_name":"NasdaqGS","created":"2021-07-22T12:33:22.000+00:00","updated":"2022-06-03T20:31:44.000+00:00","updated_pst":"2022-06-03T13:31:44.000+00:00","updated_est":"2022-06-03T16:31:44.000+00:00"},{"symbol":"AMZN","name":"Amazon.com, Inc.","type":"stock","price":"2447.0","price_open":"2484.0","price_low":"2420.98","price_high":"2488.0","price_low_high_change_percent":"2.77","price_1d":"2510.22","price_change_1d":"-63.22","price_change_percent_1d":"-2.52","price_change_percent_5d":"7.75","price_change_percent_1m":"-1.53","price_change_percent_3m":"-17.27","price_change_percent_6m":"-27.81","price_change_percent_ytd":"-28.2","price_change_percent_1y":"-23.22","price_change_percent_5y":"141.96","price_change_percent_10y":"1040.42","all_time_high_date":"2021-07-08","all_time_high_price":"3731.41","all_time_high_percent_diff":"-34.42","moving_avg_50d":"2717.9","moving_avg_50d_percent_diff":"-9.97","moving_avg_200d":"3141.81","moving_avg_200d_percent_diff":"-22.11","volume":4803542,"volume_avg_10d":5250720,"volume_avg_10d_percent_diff":"-8.52","volume_avg_3m":4302626,"volume_avg_3m_percent_diff":"11.64","pe_ratio":"58.7","market_cap":1244837838848,"eps":"41.69","week_52_high":"3773.08","week_52_high_percent_diff":"-35.15","week_52_low":"2025.2","week_52_low_percent_diff":"20.83","dividend_rate":"0.0","dividend_yield_percent":"0.0","dividend_date":None,"earnings_date":"2022-04-28","analyst_rating":"1.7 - Buy","exchange_name":"NasdaqGS","created":"2021-07-22T12:31:00.000+00:00","updated":"2022-06-03T20:31:05.000+00:00","updated_pst":"2022-06-03T13:31:05.000+00:00","updated_est":"2022-06-03T16:31:05.000+00:00"},{"symbol":"GOOG","name":"Alphabet Inc.","type":"stock","price":"2291.28","price_open":"2319.85","price_low":"2273.36","price_high":"2326.67","price_low_high_change_percent":"2.34","price_1d":"2354.92","price_change_1d":"-63.64","price_change_percent_1d":"-2.7","price_change_percent_5d":"4.35","price_change_percent_1m":"-3.02","price_change_percent_3m":"-14.7","price_change_percent_6m":"-19.62","price_change_percent_ytd":"-21.03","price_change_percent_1y":"-4.71","price_change_percent_5y":"132.93","price_change_percent_10y":"694.99","all_time_high_date":"2021-11-18","all_time_high_price":"3014.18","all_time_high_percent_diff":"-23.98","moving_avg_50d":"2470.81","moving_avg_50d_percent_diff":"-7.27","moving_avg_200d":"2726.26","moving_avg_200d_percent_diff":"-15.96","volume":1206318,"volume_avg_10d":1821070,"volume_avg_10d_percent_diff":"-33.76","volume_avg_3m":1513861,"volume_avg_3m_percent_diff":"-20.32","pe_ratio":"20.72","market_cap":1507978379264,"eps":"110.59","week_52_high":"3042.0","week_52_high_percent_diff":"-24.68","week_52_low":"2044.16","week_52_low_percent_diff":"12.09","dividend_rate":"0.0","dividend_yield_percent":"0.0","dividend_date":None,"earnings_date":None,"analyst_rating":"1.5 - Strong Buy","exchange_name":"NasdaqGS","created":"2021-07-22T12:32:25.000+00:00","updated":"2022-06-03T20:31:34.000+00:00","updated_pst":"2022-06-03T13:31:34.000+00:00","updated_est":"2022-06-03T16:31:34.000+00:00"},{"symbol":"AAPL","name":"Apple Inc.","type":"stock","price":"145.38","price_open":"146.9","price_low":"144.46","price_high":"147.95","price_low_high_change_percent":"2.41","price_1d":"151.21","price_change_1d":"-5.83","price_change_percent_1d":"-3.86","price_change_percent_5d":"-0.01","price_change_percent_1m":"-8.71","price_change_percent_3m":"-12.41","price_change_percent_6m":"-9.92","price_change_percent_ytd":"-19.91","price_change_percent_1y":"18.35","price_change_percent_5y":"299.07","price_change_percent_10y":"743.69","all_time_high_date":"2022-01-03","all_time_high_price":"181.51","all_time_high_percent_diff":"-19.91","moving_avg_50d":"159.59","moving_avg_50d_percent_diff":"-8.9","moving_avg_200d":"159.49","moving_avg_200d_percent_diff":"-8.85","volume":85749373,"volume_avg_10d":101944890,"volume_avg_10d_percent_diff":"-15.89","volume_avg_3m":98025590,"volume_avg_3m_percent_diff":"-12.52","pe_ratio":"23.69","market_cap":2353004281856,"eps":"6.14","week_52_high":"182.94","week_52_high_percent_diff":"-20.53","week_52_low":"123.85","week_52_low_percent_diff":"17.38","dividend_rate":"0.88","dividend_yield_percent":"0.58","dividend_date":"2022-05-11","earnings_date":"2022-04-28","analyst_rating":"1.9 - Buy","exchange_name":"NasdaqGS","created":"2021-07-22T04:30:22.000+00:00","updated":"2022-06-03T20:30:56.000+00:00","updated_pst":"2022-06-03T13:30:56.000+00:00","updated_est":"2022-06-03T16:30:56.000+00:00"},{"symbol":"FB","name":"Meta Platforms, Inc.","type":"stock","price":"190.78","price_open":"195.98","price_low":"189.78","price_high":"196.58","price_low_high_change_percent":"3.58","price_1d":"198.86","price_change_1d":"-8.08","price_change_percent_1d":"-4.06","price_change_percent_5d":"-0.3","price_change_percent_1m":"-10.02","price_change_percent_3m":"-6.01","price_change_percent_6m":"-37.82","price_change_percent_ytd":"-43.65","price_change_percent_1y":"-41.49","price_change_percent_5y":"24.18","price_change_percent_10y":"609.22","all_time_high_date":"2021-09-07","all_time_high_price":"382.18","all_time_high_percent_diff":"-50.08","moving_avg_50d":"205.27","moving_avg_50d_percent_diff":"-7.06","moving_avg_200d":"283.78","moving_avg_200d_percent_diff":"-32.77","volume":19086371,"volume_avg_10d":29967830,"volume_avg_10d_percent_diff":"-36.31","volume_avg_3m":32868119,"volume_avg_3m_percent_diff":"-41.93","pe_ratio":"14.44","market_cap":516311711744,"eps":"13.21","week_52_high":"384.33","week_52_high_percent_diff":"-50.36","week_52_low":"169.0","week_52_low_percent_diff":"12.89","dividend_rate":"0.0","dividend_yield_percent":"0.0","dividend_date":None,"earnings_date":"2022-04-27","analyst_rating":"2.0 - Buy","exchange_name":"NasdaqGS","created":"2021-07-22T12:32:05.000+00:00","updated":"2022-06-03T20:31:22.000+00:00","updated_pst":"2022-06-03T13:31:22.000+00:00","updated_est":"2022-06-03T16:31:22.000+00:00"}]
+}
+allkeys = data.keys()
+print(allkeys)
+
+count = data["count"]
+print(count)
+results = data["results"]
+print(results)
+print(type(results))
+listlenght= len(results)
+print(listlenght)
+x = "35.4"
+y = float(x)
+
+1/0
+
+
+
+
+for r in results:
+    print(r)
+    print(type(r))
+
+print("##############################")
+print("##############################")
+stockname = results[0:3]
+print(stockname)
+
+
+#print(type(data))
+
+
+
+print("###########################")
+mylist = [1, {"key1": "v1", "key2": "v2"}, 3, 4, 5]
+mylist = [1, {"key1": "v1", "key2": "v2"}, 3, 4, 5, 6, 7, 8]
+print(mylist[2:4])
+print(type(mylist))
+firstitem = mylist[1]
+print(type(firstitem))
+
+
+
+"""
+contenttype = headers['content-type']
+host = headers["X-RapidAPI-Host"]
+key = headers["X-RapidAPI-Key"]
+print(contenttype)
+print(host)
+print(key)
+"""
---
+++ stocks.py
@@ -0,0 +1,113 @@
+# expecting
+# AUTHORIZATION: account
+# WHERE: link to the site
+# WHAT: details of which stocks and what info we want
+# python: other python stuff which needs to be done
+
+# use functions
+# setup git
+# use aws
+
+# functions?
+# * function to create the sql
+# * function which
+
+
+import http.client
+import json
+
+
+def API(sql):
+    """
+    This function connects to the sql database and retrieves the stocks data.
+
+
+    :param sql:
+    :return: a data dictionary
+    """
+    conn = http.client.HTTPSConnection("hotstoks-sql-finance.p.rapidapi.com")
+
+    headers = {
+        'content-type': "text/plain",
+        'X-RapidAPI-Host': "hotstoks-sql-finance.p.rapidapi.com",
+        'X-RapidAPI-Key': "f3e259d6damshb4ef9605aa104b4p1efaccjsne0bcdc9b3c67"
+    }
+
+    # python: get data from website
+    conn.request("POST", "/query", sql, headers)
+    res = conn.getresponse()
+
+    # reading
+    data = res.read()
+    # decoding
+    data = data.decode("utf-8")
+    # convert to dictionary
+    data = json.loads(data)
+
+    return data
+
+
+def create_sql(symbol):
+    '''
+    This function this function takes a stock ticker symbol, inserts it in SQL and returns the text.
+
+    :param symbol: stock ticker (ex. AMZN, AAPL)
+    :return: it returns the sql data
+    '''
+
+    print(f"""You have selected {symbol}""")
+
+    sql_string = """
+    SELECT name, price, pe_ratio
+    FROM stocks
+    WHERE symbol = '""" + symbol + """'
+    ORDER BY price DESC
+    """
+
+    return sql_string
+
+
+def generate_email_text(stock_data):
+    """
+    creates a user friendly string representing the content of an email given input a dictionary (not user friendly)
+
+    :param stock_data:
+    :return:
+    """
+    # strategy: use print to get more info
+    #print(stock_data)
+    results = stock_data["results"]
+    firststock = results[0]
+    peratio = firststock["pe_ratio"]
+    price = firststock["price"]
+    price = (float(price))
+
+    name = firststock["name"]
+
+    print(peratio)
+    print(name)
+    print(price)
+
+
+print("start debugger")
+
+inputted_symbol = input("type ticker symbol here")
+
+print("before call")
+# they get SQL first
+stocks_SQl = create_sql(inputted_symbol)
+#print(stocks_SQl)
+
+# ok, they are using an API to get stocks
+stocks_dictionary = API(stocks_SQl)
+#print(stocks_dictionary)
+
+stock_info = generate_email_text(stocks_dictionary)
+
+
+
+f"""
+Hello investor, 
+You have selected the x stock and you set an alert for when it will reach x pe ratio. 
+The actual price of {5} stocs is y, pe ratio is h.
+"""
